2f451fe7b9720016753f675b303bc10973977ddd
Sakshi Rajesh Aherkar <93570056+Saraah910@users.noreply.github.com>
2023-01-08 16:45:31 +0530

    Add files via upload

---
+++ Backend/model.py
@@ -0,0 +1,13 @@
+#  @bekbrace
+#  FARMSTACK Tutorial - Sunday 13.06.2021
+
+# Pydantic allows auto creation of JSON Schemas from models
+from pydantic import BaseModel
+
+class Todo(BaseModel):
+    title: str
+    type:str
+    mobile:str
+    latitude:str
+    longitude:str
+    
